d12d2955b34d79a346b204fee2861baffc659834
amieszczanek <amieszczanek@sigma.inf.ug.edu.pl>
2011-05-05 18:35:20 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShiroiPantsu::Application.config.secret_token = '2fc9ee394bdfca360a4a9fb4ad497eec00e6d2e2c48f469b0ef8a4b73cd901f4fa07579ef597b7ca1605b420ed9d25db1f7e6d1948049a94ea6d6d95a65c9b77'
